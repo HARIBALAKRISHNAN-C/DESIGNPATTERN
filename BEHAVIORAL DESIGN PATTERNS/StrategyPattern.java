6e3aac5ca06eeae881c8a50a5ba42dc16b79c861
HARIBALAKRISHNAN-C <technohbk@gmail.com>
2024-07-28 21:36:58 +0530

    Add files via upload

---
+++ BEHAVIORAL DESIGN PATTERNS/StrategyPattern.java
@@ -0,0 +1,73 @@
+// Strategy interface
+interface PaymentStrategy {
+    void pay(double amount);
+}
+
+// Concrete Strategies
+class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+
+    public CreditCardPayment(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using Credit Card " + cardNumber);
+    }
+}
+
+class PayPalPayment implements PaymentStrategy {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using PayPal " + email);
+    }
+}
+
+class BitcoinPayment implements PaymentStrategy {
+    private String walletAddress;
+
+    public BitcoinPayment(String walletAddress) {
+        this.walletAddress = walletAddress;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid $" + amount + " using Bitcoin " + walletAddress);
+    }
+}
+
+// Context
+class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void pay(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
+
+// Demo
+public class StrategyPatternDemo {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        context.setPaymentStrategy(new CreditCardPayment("1234-5678-9012-3456"));
+        context.pay(100.00);
+
+        context.setPaymentStrategy(new PayPalPayment("example@paypal.com"));
+        context.pay(200.00);
+
+        context.setPaymentStrategy(new BitcoinPayment("1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"));
+        context.pay(300.00);
+    }
+}
