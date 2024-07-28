5568d9e2fcc606fe4ad806000e3b91f3fb56911a
HARIBALAKRISHNAN-C <technohbk@gmail.com>
2024-07-28 21:40:06 +0530

    Add files via upload

---
+++ STRUCTURAL DESIGN PATTERNS/CompositePattern.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Component
+interface FileSystemComponent {
+    void showDetails();
+}
+
+// Leaf
+class File implements FileSystemComponent {
+    private String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println
